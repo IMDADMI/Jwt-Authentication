d8c648ed71c39a0e0c62b8619d7afda3cf787bb1
IMDADMI <admiwapo@gmail.com>
2023-07-16 23:35:42 +0100

    prepare jwt

--- src/components/body/Authentication.jsx
+++ src/components/body/Authentication.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import { NavLink } from 'react-router-dom';
+import Cookies from 'js-cookie';
 export default class Authentication extends Component {
   constructor(props){
     super(props);
@@ -7,11 +8,22 @@ export default class Authentication extends Component {
       isRegistration:false,
     }
   }
-  onLoginClick(){
-    
+  onLoginClick = ()=>{
+    this.setState({isRegistration:false});
   }
-  onRegistrationClick(){
-
+  onRegistrationClick= ()=>{
+    this.setState({isRegistration:true});
+  }
+  formSubmitted = (event)=>{
+    event.preventDefault();
+    if(this.state.isRegistration){
+      //send request to /registration to create a new user
+    }else{
+      //send Request to /login to fetch the token and store it in the cookie
+      // Cookies.set('name', 'value', { expires: 7 });
+      // const name = Cookies.get('name');
+      // Cookies.remove('name');
+    }
   }
   render() {
     return (
@@ -21,7 +33,7 @@ export default class Authentication extends Component {
           <button className="mx-3 btn btn-outline-primary" onClick={this.onLoginClick}>Login</button >
           <button className="btn btn-outline-success" onClick={this.onRegistrationClick}>Registration</button >
         </div>
-        <form className="form" method='POST'>
+        <form className="form" method='POST' onSubmit={this.formSubmitted}>
           
           {this.state.isRegistration && <input className='mt-2 form-control' type="text" name="first name" id="" placeholder='first name' />}
           {this.state.isRegistration && <input className='mt-2 form-control' type="text" name="last name" id="" placeholder='last name' />}
@@ -30,6 +42,7 @@ export default class Authentication extends Component {
           <input className='mt-2 form-control' type="password" name="password" id="" placeholder='password' />
           <input type="submit" className='mt-2 btn btn-primary' value="submit" />
         </form>
+        <NavLink to='/response' className='btn btn-danger'>test</NavLink>
         </div>
       </div>
     )
